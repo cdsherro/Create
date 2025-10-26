b97cfbc180b6ebf05d9c41921c0feaca431de473
Chris Sherrod <cs7q7@umsystem.edu>
2025-10-26 18:20:53 -0500

    Initial commit

---
+++ src/Components/Create/Create.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import "./Create.css";
+
+import email_icon from '../Assets/email.jpg'
+import password_icon from '../Assets/password.png'
+import budget_icon from '../Assets/budget.png'
+
+const Create = () => {
+    return (
+
+        <div className="auth"> 
+
+            
+            <aside className="auth-media">   
+                <img src={budget_icon} alt="Welcome" />
+            </aside>
+            
+            <div className="container">
+                <div className="header">
+                    <div className="text">
+                        <mark>Login</mark>
+                    </div>
+                    <div className="highlight"></div>
+                </div>
+
+                <div className="input">
+                    <img className="icon" src={email_icon} alt="Email" />
+                    <input type="email" placeholder="Email" autoComplete="email" />
+                </div>
+
+                <div className="input">
+                    <img className="icon" src={password_icon} alt="Password" />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        autoComplete="current-password"
+                    />
+                </div>
+
+                <div className="forgot-password">
+                    Forgot Password?{" "}
+                    <button className="link" type="button">CLICK HERE!</button>
+                </div>
+
+                <div className="submit-container">
+                    <button className="submit" type="button">Submit</button>
+
+                    <div className="register group">
+                        <span className="hint">Don't have an account?</span>
+                        <button className="register-btn" type="button">Register</button>
+                    </div>
+                </div>
+            </div>
+
+               
+            <aside className="auth-right">
+                <strong>Welcome! To Task Failed Successfully Budget App.</strong>
+            </aside>
+        </div>
+    );
+};
+
+        
+
+
+export default Create;
